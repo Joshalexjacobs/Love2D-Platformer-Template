35bd2de4a7835e6259aec8cea22dd40b3d79d0eb
Joshua Alexander Jacobs <joshalexjacobs@gmail.com>
2015-10-11 19:37:21 -0700

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,10 +1,9 @@
--- Configuration file conf.lua for Disc Punks
--- https://trello.com/b/jpNY4ktw/disc-punks
+-- Configuration file conf.lua for main.lua
 
 function love.conf(t)
 	t.title = "Platformer Template" -- The title of the window the game is in (string)
 	t.version = "0.9.1"         -- The LÖVE version this game was made for (string)
-	t.window.width = 800        -- we want our game to be long and thin.
+	t.window.width = 800
 	t.window.height = 600
 
 	-- For Windows debugging
